147a78677b7ab1627d195d1e58f84ea2793fd02e
Connor Mooneyhan <connor.mooneyhan@atlanticbt.com>
2022-10-04 12:48:55 -0400

    Evaluate branch change on FugitiveChanged 🎋

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -2,14 +2,17 @@ local autocmd = vim.api.nvim_create_autocmd
 
 autocmd('BufWritePre', {pattern = '*.js', command = 'Neoformat prettier'})
 
--- implementing git branch on statusline! from https://www.reddit.com/r/neovim/comments/upe3xx/minimalist_lua_global_statusline_nvim_07_apis_lsp/
-autocmd({'BufWinEnter', 'ShellCmdPost'}, {
-  desc = 'git branch for statusline',
-  callback = function()
+local get_branch = function()
     if vim.fn.isdirectory '.git' ~= 0 then
-      vim.b.branch_name = vim.fn.system "git branch --show-current | tr -d '\n'"
+        vim.b.branch_name = vim.fn.system "git branch --show-current | tr -d '\n'"
     end
   end
+
+-- implementing git branch on statusline! from https://www.reddit.com/r/neovim/comments/upe3xx/minimalist_lua_global_statusline_nvim_07_apis_lsp/
+autocmd({'BufWinEnter'}, { callback = get_branch })
+autocmd({'User'}, {
+  pattern = 'FugitiveChanged',
+  callback = get_branch
 })
 vim.opt.laststatus = 3 -- use global statusline
 vim.opt.statusline = " %f%=%{get(b:, 'branch_name', '')} "
