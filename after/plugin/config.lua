01a4e7f1c2a2961d1df0da9148c8382912379f2d
Connor Mooneyhan <connor.mooneyhan@atlanticbt.com>
2022-10-04 15:52:55 -0400

    Say goodbye to Enter; say hello to C-Enter for cmp 👋

--- after/plugin/config.lua
+++ after/plugin/config.lua
@@ -53,7 +53,7 @@ cmp.setup {
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<Enter>'] = cmp.mapping.confirm { select = true },
+    ['<C-Enter>'] = cmp.mapping.confirm { select = true },
     ['<C-e>'] = cmp.mapping.close(),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if luasnip.expand_or_jumpable() then
