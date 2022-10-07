e66bf0ca0e200a86c255923b3b32fad41aff833d
Connor Mooneyhan <connor.mooneyhan@atlanticbt.com>
2022-10-07 16:50:40 -0400

    Remove default first arg from lg snippet ✂️:

--- after/plugin/snippets.lua
+++ after/plugin/snippets.lua
@@ -26,7 +26,7 @@ end
 
 ls.add_snippets("all", {
   s("lg", {
-    t("console.log(\""), i(1, "first"), t("\", "), f(copy_first, {1}), t(");")
+    t("console.log(\""), i(1), t("\", "), f(copy_first, {1}), t(");")
   }),
   s("st", {
     t("const ["), i(1, "state"), t(", "),
