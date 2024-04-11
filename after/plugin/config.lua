7456951e94ca07ed618762200b6bbd3d7f19c290
Connor Mooneyhan <connmoo@gmail.com>
2024-04-11 14:18:47 -0400

    Remove lsp-zero 0️⃣

--- after/plugin/config.lua
+++ after/plugin/config.lua
@@ -1,8 +1,41 @@
+-- START LSP Keymaps - https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/lsp.md#you-might-not-need-lsp-zero
+
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    -- these will be buffer-local keybindings
+    -- because they only work if you have an active language server
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end
+})
+
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local default_setup = function(server)
+  require('lspconfig')[server].setup({
+    capabilities = lsp_capabilities,
+  })
+end
+
+-- END LSP Keymaps
+
 require'mason'.setup()
 require'rust-tools'.setup {}
 
 require'mason-lspconfig'.setup {
-  automatic_installation = false,
   ensure_installed = {
     'cssmodules_ls',
     'cssls',
@@ -12,10 +45,15 @@ require'mason-lspconfig'.setup {
     'jsonls',
     'rust_analyzer',
     'lua_ls',
+    'ltex',
     'tailwindcss',
+    'texlab',
     'tsserver',
     'vimls',
     'yamlls'
+  },
+  handlers = {
+    default_setup,
   }
 }
 
@@ -100,10 +138,14 @@ cmp.setup.cmdline('/', {
   }
 })
 
-local lsp = require'lsp-zero'
-lsp.preset'lsp-only'
-lsp.nvim_workspace()
-lsp.setup()
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+-- local lsp = require'lsp-zero'
+-- lsp.preset'lsp-only'
+-- lsp.nvim_workspace()
+-- lsp.setup()
 
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
