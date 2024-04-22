db428d1f3c544c5f5dfb83f5b79eb27ed093ed3e
Connor Mooneyhan <connmoo@gmail.com>
2024-04-22 07:30:11 -0400

    em=\emptyset

--- after/plugin/snippets.lua
+++ after/plugin/snippets.lua
@@ -66,6 +66,9 @@ ls.add_snippets("tex", {
   s("cal", { -- \mathcal
     t("\\mathcal{"), i(1), t("}")
   }),
+  s("em", { -- \emptyset
+    t("\\emptyset")
+  }),
   s("fr", { -- fraction
     t("\\frac{"), i(1), t("}{"), i(2), t("}"), i(0)
   }),
