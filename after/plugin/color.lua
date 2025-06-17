d49075d1fce12a96497d8b654f97661b098ee631
Connor Mooneyhan <connor.mooneyhan@atlanticbt.com>
2025-06-17 12:25:45 -0400

    switch to lazy.nvim

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,6 +1,17 @@
-vim.cmd("colorscheme rose-pine")
+require('rose-pine').setup({
+  disable_background = true
+})
 
-local unused = true
+function ColorMyPencils(color)
+  color = color or "rose-pine"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorMyPencils()
+
 -- local colors = require('catppuccin.palettes').get_palette()
 
 vim.cmd('hi DiagnosticUnderlineError gui=undercurl')
